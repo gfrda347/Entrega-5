752ae16b6e5d2e6e9b87798cd2cd01b3966a3445
Santiago <105825670+gfrda347@users.noreply.github.com>
2024-05-26 21:25:30 -0500

    Delete mi_flask.py

--- mi_flask.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    # Aquí se renderiza el archivo de plantilla 'index.html'
-    return render_template('index.html')
